81de51b5a51157b8195b2a46ff8d42c05d37e5ee
Steve Steele <steven.harville.steele@gmail.com>
2021-07-05 12:11:04 -0500

    Add songs, lines

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
-const notes = document.getElementById('notes');
+const songId = 'asserin-asseran';
+const songs = [
+    {
+        id: 'asserin-asseran',
+        name: 'Asserin Asseran',
+        notes: [
+            [1, 1, 4, 0, 1, 1, 6],
+            [1, 1, 2, 1, 2, 3, 4],
+        ],
+    },
+];
+const song = songs.find((s) => {
+    return s.id === songId;
+});
 const bells = [
+    {
+        color: null,
+        index: 0,
+        note: null,
+    },
     {
         color: 'f00',
         index: 1,
@@ -42,20 +60,44 @@ const bells = [
         note: 'c',
     },
 ];
+const notes = document.getElementById('notes');
+
+createLineElement = () => {
+    const el = document.createElement('div');
+    el.classList.add('line');
+    return el;
+};
 
 createBellElement = (bell) => {
     const el = document.createElement('div');
     el.classList.add('circle', bell.note);
     return el;
-}
+};
 
-renderNote = (bells, index = 1) => {
+renderNote = (lineEl, bells, index = 1) => {
     const bell = bells.find((b) => {
         return b.index === index;
     })
-    notes.append(createBellElement(bell));
-}
+    return notes.append(createBellElement(bell));
+};
 
-renderNote(bells, 1);
-renderNote(bells, 1);
-renderNote(bells, 4);
+renderLine = () => {
+    return notes.append(createLineElement());
+};
+
+renderSong = (song) => {
+    for (let lines of song) {
+        const lineEl = renderLine();
+        let note;
+        if (!Array.isArray(lines)) {
+            note = lines;
+            renderNote(lineEl, bells, note);
+        } else {
+            for (note of lines) {
+                renderNote(lineEl, bells, note);
+            }
+        }
+    }
+};
+
+renderSong(song.notes);
