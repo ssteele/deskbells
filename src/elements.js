60fbd540f5ad889546a54608128002e6eff4e839
Steve Steele <steven.harville.steele@gmail.com>
2021-07-06 13:44:41 -0500

    Add elements

---
+++ src/elements.js
@@ -0,0 +1,19 @@
+
+export const createLineElement = () => {
+    const el = document.createElement('div');
+    el.classList.add('line');
+    return el;
+};
+
+export const createNoteElement = (bell) => {
+    const el = document.createElement('div');
+    el.classList.add('note', bell.note);
+    return el;
+};
+
+export const createLyricElement = (text = null) => {
+    const el = document.createElement('div');
+    el.classList.add('lyric');
+    el.innerHTML = text || '&nbsp;';
+    return el;
+};
