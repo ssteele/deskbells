5a4223cae8561e2f6d597e601ed751c076c3c2fb
Steve Steele <steven.harville.steele@gmail.com>
2021-07-11 15:37:00 -0500

    Update songs to match new notesMap paradigm

--- src/songs.js
+++ src/songs.js
@@ -4,27 +4,27 @@ export const songs = [
         name: 'Twinkle Twinkle',
         lines: [
             {
-                notes: [1, 1, 5, 5, 6, 6, 5],
+                notes: [1, 1, 8, 8, 10, 10, 8],
                 lyrics: ['Twin-', '-kle', 'twin-', '-kle', 'lit-', '-tle', 'star'],
             },
             {
-                notes: [4, 4, 3, 3, 2, 2, 1],
+                notes: [6, 6, 5, 5, 3, 3, 1],
                 lyrics: ['How', 'I', 'won-', '-der', 'what', 'you', 'are'],
             },
             {
-                notes: [5, 5, 4, 4, 3, 3, 2],
+                notes: [8, 8, 6, 6, 5, 5, 3],
                 lyrics: ['Up', 'abo-', '-ve', 'the', 'world', 'so', 'high'],
             },
             {
-                notes: [5, 5, 4, 4, 3, 3, 2],
+                notes: [8, 8, 6, 6, 5, 5, 3],
                 lyrics: ['Like', 'a', 'dia-', '-mond', 'in', 'the', 'sky'],
             },
             {
-                notes: [1, 1, 5, 5, 6, 6, 5],
+                notes: [1, 1, 8, 8, 10, 10, 8],
                 lyrics: ['Twin-', '-kle', 'twin-', '-kle', 'lit-', '-tle', 'star'],
             },
             {
-                notes: [4, 4, 3, 3, 2, 2, 1],
+                notes: [6, 6, 5, 5, 3, 3, 1],
                 lyrics: ['How', 'I', 'won-', '-der', 'what', 'you', 'are'],
             },
         ],
@@ -34,35 +34,35 @@ export const songs = [
         name: 'Lightly Row',
         lines: [
             {
-                notes: [5, 3, 3, 0, 4, 2, 2],
+                notes: [8, 5, 5, 0, 6, 3, 3],
                 lyrics: [],
             },
             {
-                notes: [1, 2, 3, 4, 5, 5, 5],
+                notes: [1, 3, 5, 6, 8, 8, 8],
                 lyrics: [],
             },
             {
-                notes: [5, 3, 3, 0, 4, 2, 2],
+                notes: [8, 5, 5, 0, 6, 3, 3],
                 lyrics: [],
             },
             {
-                notes: [1, 3, 5, 5, 1, 1, 1],
+                notes: [1, 5, 8, 8, 1, 1, 1],
                 lyrics: [],
             },
             {
-                notes: [2, 2, 2, 2, 2, 3, 4],
+                notes: [3, 3, 3, 3, 3, 5, 6],
                 lyrics: [],
             },
             {
-                notes: [3, 3, 3, 3, 3, 4, 5],
+                notes: [5, 5, 5, 5, 5, 6, 8],
                 lyrics: [],
             },
             {
-                notes: [5, 3, 3, 0, 4, 2, 2],
+                notes: [8, 5, 5, 0, 6, 3, 3],
                 lyrics: [],
             },
             {
-                notes: [1, 3, 5, 5, 1, 1, 1],
+                notes: [1, 5, 8, 8, 1, 1, 1],
                 lyrics: [],
             },
         ],
@@ -72,27 +72,27 @@ export const songs = [
         name: 'Aserrin Aserran',
         lines: [
             {
-                notes: [5, 5, 8, 0, 5, 5, 3],
+                notes: [8, 8, 15, 0, 8, 8, 5],
                 lyrics: ['As-', '-er-', '-rin', null, 'as-', '-er-', '-ran'],
             },
             {
-                notes: [5, 5, 6, 5, 6, 7, 8],
+                notes: [8, 8, 10, 8, 10, 13, 15],
                 lyrics: ['Los', 'ma-', '-der-', '-os', 'de', 'San', 'Juan'],
             },
             {
-                notes: [5, 5, 8, 0, 5, 5, 3],
+                notes: [8, 8, 15, 0, 8, 8, 5],
                 lyrics: ['Pi-', '-den', 'pan', null, 'no', 'les', 'dan'],
             },
             {
-                notes: [5, 5, 6, 5, 6, 7, 8],
+                notes: [8, 8, 10, 8, 10, 13, 15],
                 lyrics: ['Pi-', '-den', 'que-', '-so', 'les', 'dan', 'hueso'],
             },
             {
-                notes: [5, 5, 8, 0, 5, 5, 3],
+                notes: [8, 8, 15, 0, 8, 8, 5],
                 lyrics: ['Pi-', '-den', 'vi-', '-no', 'si', 'les', 'dan'],
             },
             {
-                notes: [5, 5, 6, 5, 6, 7, 8],
+                notes: [8, 8, 10, 8, 10, 13, 15],
                 lyrics: ['Se', 'ma-', '-re-', '-an', 'y', 'se', 'van'],
             },
         ],
@@ -102,35 +102,35 @@ export const songs = [
         name: 'Yesterday',
         lines: [
             {
-                notes: [1, 7, 3, 6, 6, 5, 4],
+                notes: [1, 13, 5, 10, 10, 8, 6],
                 lyrics: ['Yesterday', 'all my', 'trouble seemed', 'so far', 'away', '', 'now it'],
             },
             {
-                notes: [5, 1, 1, 7, 6, 2, 4, 1],
+                notes: [8, 1, 1, 13, 10, 3, 6, 1],
                 lyrics: ['looks as though they\'re', 'here to', 'stay', '', 'oh I believe', 'in', 'yes-', '-terday'],
             },
             {
-                notes: [1, 7, 3, 6, 6, 5, 4],
+                notes: [1, 13, 5, 10, 10, 8, 6],
                 lyrics: ['Suddenly', 'I\'m not', 'half the man', 'I used to', 'be', '', 'there\'s a'],
             },
             {
-                notes: [5, 1, 1, 7, 6, 2, 4, 1],
+                notes: [8, 1, 1, 13, 10, 3, 6, 1],
                 lyrics: ['shadow hanging', 'over me', '', 'oh', 'yesterday', 'came', 'sud-', '-denly'],
             },
             {
-                notes: [7, 3, 6, 3, 4, 2, 5, 1],
+                notes: [13, 5, 10, 5, 6, 3, 8, 1],
                 lyrics: ['Why', 'she', 'had', 'to', 'go I don\'t', 'know she', 'wouldn\'t', 'say'],
             },
             {
-                notes: [7, 3, 6, 3, 4, 2, 5, 1],
+                notes: [13, 5, 10, 5, 6, 3, 8, 1],
                 lyrics: ['I', 'said', 'some-', '-thing', 'wrong now I', 'long for', 'yesterday', '-ay -ay -ay'],
             },
             {
-                notes: [1, 7, 3, 6, 6, 5, 4],
+                notes: [1, 13, 5, 10, 10, 8, 6],
                 lyrics: ['Yesterday', 'love was', 'such an easy', 'game to', 'play', '', 'now I'],
             },
             {
-                notes: [5, 1, 1, 7, 6, 2, 4, 1],
+                notes: [8, 1, 1, 13, 10, 3, 6, 1],
                 lyrics: ['need a place to', 'hide away', '', 'oh', 'I belie-', '-ve in', 'yes-', '-terday'],
             },
         ],
