105ab4016193717c9d34e81cc963157f9eb27565
Steve Steele <steven.harville.steele@gmail.com>
2021-07-11 15:34:40 -0500

    Move bells.js content to instruments.js

--- src/instruments.js
+++ src/instruments.js
@@ -2,9 +2,85 @@ export const instruments = [
     {
         id: 'deskbells',
         name: 'Desk Bells',
+        notesMap: [
+            {
+                index: 0,
+                note: 'rest',
+            },
+            {
+                index: 1,
+                note: 'c',
+            },
+            {
+                index: 3,
+                note: 'd',
+            },
+            {
+                index: 5,
+                note: 'e',
+            },
+            {
+                index: 6,
+                note: 'f',
+            },
+            {
+                index: 8,
+                note: 'g',
+            },
+            {
+                index: 10,
+                note: 'a',
+            },
+            {
+                index: 12,
+                note: 'b',
+            },
+            {
+                index: 13,
+                note: 'c',
+            },
+        ],
     },
     {
         id: 'xylophone',
         name: 'Xylophone',
+        notesMap: [
+            {
+                index: 0,
+                note: 'rest',
+            },
+            {
+                index: 1,
+                note: 'c',
+            },
+            {
+                index: 3,
+                note: 'd',
+            },
+            {
+                index: 5,
+                note: 'e',
+            },
+            {
+                index: 6,
+                note: 'f',
+            },
+            {
+                index: 8,
+                note: 'g',
+            },
+            {
+                index: 10,
+                note: 'a',
+            },
+            {
+                index: 12,
+                note: 'b',
+            },
+            {
+                index: 13,
+                note: 'c',
+            },
+        ],
     },
 ];
